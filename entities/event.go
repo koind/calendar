6827410f388c515bfd41cf91fd128d4555f5b196
Damir15 <krosava_15d@mail.ru>
2019-08-17 18:11:58 +0600

    Added new entity

---
+++ entities/event.go
@@ -0,0 +1,13 @@
+package entities
+
+import "time"
+
+type Event struct {
+	UUID           int           `json:"uuid"`                       //UUID - уникальный идентификатор события
+	Title          string        `json:"title"`                      //Заголовок - короткий текст
+	Datetime       time.Time     `json:"datetime"`                   //Дата и время события
+	Duration       time.Duration `json:"duration"`                   //Длительность события
+	Description    string        `json:"description,omitempty"`      //Описание события, опционально
+	UserId         int           `json:"user_id"`                    //Пользователь, владелец события
+	TimeSendNotify time.Time     `json:"time_send_notify,omitempty"` //За сколько времени высылать уведомление, опционально
+}
