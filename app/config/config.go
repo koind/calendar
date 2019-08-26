e10254813ee6fa333370330f27933910a4b4b08a
Damir15 <krosava_15d@mail.ru>
2019-08-26 23:04:52 +0600

    Добавил gRPC клиента

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 type Config struct {
-	Host string `env:"CALENDAR_HOST,required"`
-	Port int    `env:"CALENDAR_PORT" envDefault:"7777"`
+	Host          string `env:"CALENDAR_HOST,required"`
+	Port          int    `env:"CALENDAR_PORT" envDefault:"7777"`
+	ClientTimeout int    `env:"CLIENT_TIMEOUT" envDefault:"400"`
 }
---
+++ cmd/grpc_client.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/caarlos0/env"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/koind/calendar/app/config"
+	"github.com/koind/calendar/app/delivery/grpc/pb"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func main() {
+	cfg := config.Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	timeout := time.Duration(cfg.ClientTimeout) * time.Millisecond
+	calendarDomain := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	clientConn, err := grpc.Dial(calendarDomain, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer clientConn.Close()
+
+	client := pb.NewEventClient(clientConn)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	timestamp, err := ptypes.TimestampProto(time.Now())
+	if err != nil {
+		log.Println(err)
+	}
+
+	notifyTime, err := ptypes.TimestampProto(time.Now().Add(100))
+	if err != nil {
+		log.Println(err)
+	}
+
+	request := &pb.EventRequest{
+		Title:          "To buy a car",
+		Datetime:       timestamp,
+		Duration:       ptypes.DurationProto(100),
+		UserId:         123,
+		TimeSendNotify: notifyTime,
+	}
+	response, err := client.Create(ctx, request)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Printf("%+v", response)
+
+	event := &pb.EventChange{
+		EventID: &pb.EventID{
+			Uuid: response.Uuid,
+		},
+		Request: &pb.EventRequest{Title: "Buy Audi RS6"},
+	}
+	response, err = client.Update(ctx, event)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Printf("%+v", response)
+
+	eventID := &pb.EventID{
+		Uuid: response.Uuid,
+	}
+	status, err := client.Delete(ctx, eventID)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Println(status.Status)
+}
