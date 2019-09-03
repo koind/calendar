07483c08be47b0b0d35a4d2dd4dbfad6f0a46db7
Damir15 <krosava_15d@mail.ru>
2019-09-03 23:28:27 +0600

    Ура! Удалось завернуть в контейнер

--- cmd/server/http_server.go
+++ cmd/server/http_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/caarlos0/env"
+	"github.com/koind/calendar/app/adapter"
 	"github.com/koind/calendar/app/config"
 	"github.com/koind/calendar/app/domain/service"
 	"github.com/koind/calendar/app/storage/memory"
@@ -31,4 +32,18 @@ func main() {
 	hs := httpServer.NewHTTPServer(httpEventService, calendarDomain)
 
 	logger.Error("Error starting app", zap.Error(hs.Start()))
+
+	dsn := "postgres://root:123123@postgres:5432/events?sslmode=disable"
+	op := adapter.DBOptions{
+		DSN:             dsn,
+		PingTimeout:     430,
+		MaxOpenConns:    10,
+		MaxIdleConns:    5,
+		ConnMaxLifetime: 15,
+	}
+	db, err := adapter.IntDB(op)
+	if err != nil {
+		logger.Debug("dont connection", zap.Error(err))
+	}
+	logger.Info("db", zap.Any("db", db))
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/caarlos0/env v3.5.0+incompatible // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/mux v1.7.3
-	github.com/jackc/pgx v3.5.0+incompatible // indirect
+	github.com/jackc/pgx v3.5.0+incompatible
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/pkg/errors v0.8.1
 	go.uber.org/atomic v1.4.0 // indirect
